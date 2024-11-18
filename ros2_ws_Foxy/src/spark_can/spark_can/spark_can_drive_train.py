c21e86cebf8a8b0edbda49fea3207f41a9a82597
AndBoun <167598896+AndBoun@users.noreply.github.com>
2024-11-18 00:22:12 -0600

    refactor strpub to remove unnecessary float casting for position values

--- ros2_ws_Foxy/src/spark_can/spark_can/spark_can_drive_train.py
+++ ros2_ws_Foxy/src/spark_can/spark_can/spark_can_drive_train.py
@@ -121,10 +121,10 @@ spark_DrillActuator = sparkBus.init_controller(17)
 def strpub():
     steerMsg = Steer()
     """ ### WAS ORIGINALLY CASTED TO INT###"""
-    steerMsg.lf = float(int(spark_str_lf.position))
-    steerMsg.lb = float(int(spark_str_lb.position))
-    steerMsg.rf = float(int(spark_str_rf.position))
-    steerMsg.rb = float(int(spark_str_rb.position))
+    steerMsg.lf = (int(spark_str_lf.position))
+    steerMsg.lb = (int(spark_str_lb.position))
+    steerMsg.rf = (int(spark_str_rf.position))
+    steerMsg.rb = (int(spark_str_rb.position))
     str_pub.publish(steerMsg)
 
 
