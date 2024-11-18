00318cb1354dd0b68f77ed79cc0e1b64b2d0ccbe
AndBoun <167598896+AndBoun@users.noreply.github.com>
2024-11-18 16:24:12 -0600

    add Status to __all__ in spark_can module for improved accessibility

--- ros2_ws_Foxy/src/spark_can/spark_can/__init__.py
+++ ros2_ws_Foxy/src/spark_can/spark_can/__init__.py
@@ -1,9 +1,11 @@
 # spark_can/spark_can/__init__.py
 from .spark_controller import Controller
 from .spark_can import SparkBus
+from .Statuses import Status
 
 
 __all__ = [
     'SparkBus',
     'Controller'
+    'Status'
 ]
