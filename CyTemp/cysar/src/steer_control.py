95ec13bc21493eca9759a6520d2738a30a17674c
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-09-24 02:30:34 +0000

    add logging

--- CyTemp/cysar/src/steer_control.py
+++ CyTemp/cysar/src/steer_control.py
@@ -1,5 +1,6 @@
 from cysar.msg import SteerTrain
 from SparkCANLib.SparkCAN import SparkBus
+import rospy
 
 # CAN IDs for Drive Controllers
 FLS = 7
@@ -19,9 +20,13 @@ class SteerControl():
     def __init__(self, bus : SparkBus):
         self.bus = bus
         self.FLMotor = self.bus.init_controller(FLS)
+        rospy.loginfo("Initialized Front Left Steer (FLS) controller with CAN ID %d", FLS)
         self.FRMotor = self.bus.init_controller(FRS)
+        rospy.loginfo("Initialized Front Right Steer (FRS) controller with CAN ID %d", FRS)
         self.BLMotor = self.bus.init_controller(BLS)
+        rospy.loginfo("Initialized Back Left Steer (BLS) controller with CAN ID %d", BLS)
         self.BRMotor = self.bus.init_controller(BRS)
+        rospy.loginfo("Initialized Back Right Steer (BRS) controller with CAN ID %d", BRS)
 
     def set_velocity(self, msg : SteerTrain):
         """
@@ -30,7 +35,11 @@ class SteerControl():
         Args:
             msg (DriveTrain): The values from ROS indicating the velocity of each motor.
         """
+        rospy.loginfo("Setting Front Left Motor velocity to %f", msg.front_left)
         self.FLMotor.percent_output(msg.front_left)
+        rospy.loginfo("Setting Front Right Motor velocity to %f", msg.front_right)
         self.FRMotor.percent_output(msg.front_right)
+        rospy.loginfo("Setting Back Left Motor velocity to %f (inverted: %f)", msg.back_left, INVERTED * msg.back_left)
         self.BLMotor.percent_output(INVERTED * msg.back_left)
+        rospy.loginfo("Setting Back Right Motor velocity to %f (inverted: %f)", msg.back_right, INVERTED * msg.back_right)
         self.BRMotor.percent_output(INVERTED * msg.back_right)
