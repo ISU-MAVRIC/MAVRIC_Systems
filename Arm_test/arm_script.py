0908acc9611f86a57b0c4dfc3d48326a7d4417cc
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-09-29 23:39:46 +0000

    use while loop for percentage

--- Arm_test/arm_script.py
+++ Arm_test/arm_script.py
@@ -1,7 +1,9 @@
 from SparkCANLib import SparkController, SparkCAN
+import numpy as np
 
 bus = SparkCAN.SparkBus(channel="can0", bustype='socketcan', bitrate=1000000)
 
 arm_motor = bus.init_controller(5)
 
-arm_motor.percent_output(0.05)
+while (True):
+    arm_motor.percent_output(np.float32(0.05))
