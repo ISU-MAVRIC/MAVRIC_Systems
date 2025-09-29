96c58aa9244c00e43a575066fef01ec914eae543
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-09-29 23:49:56 +0000

    update can ID

--- Arm_test/arm_script.py
+++ Arm_test/arm_script.py
@@ -2,9 +2,9 @@ from SparkCANLib import SparkController, SparkCAN
 
 bus = SparkCAN.SparkBus(channel="can0", bustype='socketcan', bitrate=1000000)
 
-arm_motor = bus.init_controller(5)
-l_motor = bus.init_controller(1)
+arm_motor = bus.init_controller(12)
+# l_motor = bus.init_controller(1)
 
 while (True):
     arm_motor.percent_output(0.05)
-    l_motor.percent_output(0.05)
+    # l_motor.percent_output(0.05)
