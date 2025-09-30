849a7099124d8419db63472cda302d9fead17a47
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-09-30 00:04:38 +0000

    transfer

--- Arm_test/arm_script.py
+++ Arm_test/arm_script.py
@@ -6,7 +6,7 @@ arm_motor = bus.init_controller(12)
 # l_motor = bus.init_controller(1)
 
 while (True):
-    arm_motor.percent_output(0.05)
+    arm_motor.percent_output(0.10)
 
     # print (arm_motor.position)
 
