87f0613297daf55db0236e8208a4f86526f29109
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-10-02 18:26:07 -0500

    test

--- Arm_test/servo_adafruit.py
+++ Arm_test/servo_adafruit.py
@@ -3,8 +3,8 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
-kit.continuous_servo[1].throttle = 1
-time.sleep(15)
+# kit.continuous_servo[1].throttle = 1
+# time.sleep(15)
 kit.continuous_servo[1].throttle = -1
 time.sleep(15)
 kit.continuous_servo[1].throttle = 0
