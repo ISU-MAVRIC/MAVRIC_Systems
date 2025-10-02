13e68cf472e1946bf1d1efef1adedb4518e59835
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-10-02 18:17:19 -0500

    test

--- Arm_test/can_test.py
+++ Arm_test/can_test.py
@@ -8,3 +8,10 @@ motor.percent_output(-0.40)
 
 while True:
     pass
+
+
+
+# import rev
+
+# motor = rev.SparkMax(1, rev.SparkMax.MotorType.kBrushless)
+# motor.set(0.5)  # run at 50% power
--- Arm_test/servo_adafruit.py
+++ Arm_test/servo_adafruit.py
@@ -1,15 +1,15 @@
 import time
-from adafruit_lib import ServoKit
+from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
-kit.continuous_servo[0].throttle = 1
-time.sleep(1)
-kit.continuous_servo[0].throttle = -1
-time.sleep(1)
-kit.continuous_servo[0].throttle = 0
-
-
-# kit.servo[0].angle = 180
+# kit.continuous_servo[0].throttle = 1
 # time.sleep(1)
-# kit.servo[0].angle = 0
+# kit.continuous_servo[0].throttle = -1
+# time.sleep(1)
+# kit.continuous_servo[0].throttle = 0
+
+
+kit.servo[0].angle = 180
+time.sleep(1)
+kit.servo[0].angle = 0
