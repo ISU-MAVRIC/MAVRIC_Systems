2f59671134e41b54e6a493a703e70c2ce78752f5
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-10-02 21:13:19 -0500

    rest

--- Arm_test/can_test.py
+++ Arm_test/can_test.py
@@ -7,7 +7,7 @@ motor = bus.init_controller(12)
 # motor3 = bus.init_controller(9)
 # motor4 = bus.init_controller(8)
 
-motor.percent_output(0.30)
+motor.percent_output(0.40)
 # motor2.percent_output(0.05)
 # motor3.percent_output(0.05)
 # motor4.percent_output(-0.05)
