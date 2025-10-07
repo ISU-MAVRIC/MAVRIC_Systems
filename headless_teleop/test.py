a000f308070dbfdaa2337edc1d5d1c4948804459
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-10-06 20:24:16 -0500

    test

--- headless_teleop/test.py
+++ headless_teleop/test.py
@@ -12,7 +12,7 @@ BLS = bus.init_controller(BLS_ID)
 BRS = bus.init_controller(BRS_ID)
 
 # FLS.position_output(0.5)
-FLS.percent_output(0.2)
+FLS.percent_output(0.01)
 # time.sleep(5)
 # FRS.position_output(0.8)
 # BLS.position_output(-0.8)
