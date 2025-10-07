7a0e299a2f24980f409de894b644b74bb582c860
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-10-06 19:34:50 -0500

    test

--- Arm_test/can_test.py
+++ Arm_test/can_test.py
@@ -2,12 +2,12 @@ from SparkCANLib import SparkController, SparkCAN
 
 bus = SparkCAN.SparkBus(channel="can0", bustype='socketcan', bitrate=1000000)
 
-motor = bus.init_controller(15)
+motor = bus.init_controller(1)
 # motor2 = bus.init_controller(10)
 # motor3 = bus.init_controller(9)
 # motor4 = bus.init_controller(8)
 
-motor.percent_output(-0.40)
+motor.percent_output(0.1)
 # motor2.percent_output(0.05)
 # motor3.percent_output(0.05)
 # motor4.percent_output(-0.05)
