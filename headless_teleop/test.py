cd2004a1c53897c5a758ff3b93a3903ec8c471b2
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-10-06 20:07:58 -0500

    test

--- headless_teleop/test.py
+++ headless_teleop/test.py
@@ -7,15 +7,11 @@ from  config import *
 
 bus = SparkBus()
 
-FLD = Controller(bus, FLD_ID)
-FRD = Controller(bus, FRD_ID)
-BLD = Controller(bus, BLD_ID)
-BRD = Controller(bus, BRD_ID)
-
-FLD.percent_output(0.1)
-FRD.percent_output(0.1)
-BLD.percent_output(-0.1)
-BRD.percent_output(-0.1)
+FLS = bus.init_controller(FLS_ID)
+FRS = bus.init_controller(FRS_ID)
+BLS = bus.init_controller(BLS_ID)
+BRS = bus.init_controller(BRS_ID)
 
 while True:
-    pass
+    print(f"FLS.position: {FLS.position}, FRS.position: {FRS.position}, BLS.position: {BLS.position}, BRS.position: {BRS.position}")
+    time.sleep(0.1)
