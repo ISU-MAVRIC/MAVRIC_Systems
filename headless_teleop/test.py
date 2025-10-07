cb91013b6ee207013dd4dbb4abed6bab34dc6969
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-10-06 20:15:20 -0500

    test

--- headless_teleop/test.py
+++ headless_teleop/test.py
@@ -11,6 +11,11 @@ FRS = bus.init_controller(FRS_ID)
 BLS = bus.init_controller(BLS_ID)
 BRS = bus.init_controller(BRS_ID)
 
+FLS.setposition(0.8)
+FRS.setposition(0.8)
+BLS.setposition(-0.8)
+BRS.setposition(-0.8)
+
 while True:
     print(f"FLS.position: {FLS.position}, FRS.position: {FRS.position}, BLS.position: {BLS.position}, BRS.position: {BRS.position}")
     time.sleep(0.1)
