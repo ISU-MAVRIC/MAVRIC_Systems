a156231b9cf886347c6842739adf92c6fe2cc72e
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-11-10 17:56:41 -0600

    add jetson moisture sensor code

---
+++ rasp-test/moisture_test_jetson.py
@@ -0,0 +1,10 @@
+import board
+import busio
+from adafruit_ms8607 import MS8607
+
+i2c = busio.I2C(7)
+sensor = MS8607(i2c)
+
+print("Pressure: %.2f hPa" % sensor.pressure)
+print("Temperature: %.2f C" % sensor.temperature)
+print("Humidity: %.2f %% rH" % sensor.relative_humidity)
