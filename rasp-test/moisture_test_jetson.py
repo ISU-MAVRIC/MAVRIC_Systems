d0f07a5509a348ef18b088a86d159b72ec9f2622
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-11-11 19:57:52 -0600

    re add both sensors

--- rasp-test/moisture_test_jetson.py
+++ rasp-test/moisture_test_jetson.py
@@ -8,12 +8,12 @@ i2c_device.I2CDevice._I2CDevice__probe_for_device = _no_probe  # override the ma
 import board, busio
 from adafruit_ms8607 import MS8607
 
-# i2c = busio.I2C(board.SCL, board.SDA)
-# sensor = MS8607(i2c)
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = MS8607(i2c)
 
-# print("Pressure: %.2f hPa" % sensor.pressure)
-# print("Temperature: %.2f C" % sensor.temperature)
-# print("Humidity: %.2f %% rH" % sensor.relative_humidity)
+print("Pressure: %.2f hPa" % sensor.pressure)
+print("Temperature: %.2f C" % sensor.temperature)
+print("Humidity: %.2f %% rH" % sensor.relative_humidity)
 
 
 i2c_2 = busio.I2C(board.SCL_1, board.SDA_1)
