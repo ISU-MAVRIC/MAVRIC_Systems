0d59dfdeb562b52a5de745f64dcdc1b70c66ee19
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-11-11 19:31:19 -0600

    test

--- rasp-test/moisture_test_jetson.py
+++ rasp-test/moisture_test_jetson.py
@@ -1,12 +1,16 @@
 import adafruit_bus_device.i2c_device as i2c_device
-i2c_device.I2CDevice.__probe_for_device = lambda self: None  # disable zero-length write check
+
+# Properly override BEFORE any instance is created
+def _no_probe(self):
+    pass
+i2c_device.I2CDevice._I2CDevice__probe_for_device = _no_probe  # override the mangled private name
 
 import board, busio
 from adafruit_ms8607 import MS8607
 
-i2c = busio.I2C(board.SCL, board.SDA)  # Jetson uses /dev/i2c-1
+i2c = busio.I2C(board.SCL, board.SDA)
 sensor = MS8607(i2c)
 
-print("Pressure:", sensor.pressure, "hPa")
-print("Temperature:", sensor.temperature, "°C")
-print("Humidity:", sensor.relative_humidity, "% rH")
+print("Pressure: %.2f hPa" % sensor.pressure)
+print("Temperature: %.2f C" % sensor.temperature)
+print("Humidity: %.2f %% rH" % sensor.relative_humidity)
