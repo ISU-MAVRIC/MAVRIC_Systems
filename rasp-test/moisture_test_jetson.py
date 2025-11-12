c5dc80a6284c20710abf5d192c644ce3ef8496f7
AndBoun <167598896+AndBoun@users.noreply.github.com>
2025-11-11 19:30:41 -0600

    test

--- rasp-test/moisture_test_jetson.py
+++ rasp-test/moisture_test_jetson.py
@@ -1,11 +1,10 @@
 import adafruit_bus_device.i2c_device as i2c_device
+i2c_device.I2CDevice.__probe_for_device = lambda self: None  # disable zero-length write check
+
 import board, busio
 from adafruit_ms8607 import MS8607
 
-# --- patch to disable probing ---
-i2c_device.I2CDevice.__probe_for_device = lambda self: None
-
-i2c = busio.I2C(board.SCL, board.SDA)  # use i2c-1
+i2c = busio.I2C(board.SCL, board.SDA)  # Jetson uses /dev/i2c-1
 sensor = MS8607(i2c)
 
 print("Pressure:", sensor.pressure, "hPa")
